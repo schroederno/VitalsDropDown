c1425e1b63bbc5a50a5a1a93e4f408c6c674f8cf
Schroeder <nschroeder@pv.local>
2017-06-15 11:52:51 -0500

    INIT

---
+++ DropBoxHeightWeightPrototype/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropBoxHeightWeightPrototype
+{
+    public class MainViewModel
+    {
+     
+        public virtual string outputTextBox { get; set; }
+        public virtual int outputFeet { get; set; }
+        public virtual int outputInches { get; set; }
+        public virtual bool AdvancedFormat { get; set; } = true;
+        public virtual bool inInches { get; set; }
+        public virtual bool inCentimeters { get; set; }
+        public void btnPressed(string character)
+        {
+            outputTextBox += character;
+        }
+
+        public void btnClear()
+        {
+            outputTextBox = string.Empty;
+        }
+
+        public void btnBackspace()
+        {
+            string temp = outputTextBox;
+            if(temp.Length>0)
+                outputTextBox = temp.Substring(0, temp.Length - 1);
+        }
+        public void OnoutputTextBoxChanged()
+        {
+            
+        }
+        public void OnoutputFeetChanged()
+        {
+            int value = outputFeet * 12 + outputInches;
+            outputTextBox = value.ToString();
+        }
+        public void OnoutputInchesChanged()
+        {
+            int value = outputFeet * 12 + outputInches;
+            outputTextBox = value.ToString();
+        }
+    }
+}
